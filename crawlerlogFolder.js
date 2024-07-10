86061858b52d2e3514385bc5cf938da8f374ec77
Ravikant tiwari <115395652+theravikanttiwari@users.noreply.github.com>
2024-07-10 19:31:09 +0530

    Create crawlerlogFolder.js

---
+++ crawlerlogFolder.js
@@ -0,0 +1,15 @@
+function minOperations(logs) {
+    let depth = 0;
+    
+    for (let log of logs) {
+        if (log === "../") {
+            if (depth > 0) {
+                depth--;
+            }
+        } else if (log !== "./") {
+            depth++;
+        }
+    }
+    
+    return depth;
+}
